b7bcb655cf07a1747833b1b502f96799a59ad674
manuelafedi <91663595+manuelafedi@users.noreply.github.com>
2021-11-13 11:28:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   int n=0
+      cin>>n
+      
+      if(n<2){
+         cout<< "errore"<<endl;
+         
+      }else{
+         cout<<"1"<<endl;
+         int a=0, b=1, c=0;
+         for (int 1=0; 1<=(n=2); 1++);
+         {cout c=a+b;
+                 a=b;
+                 b=c;
+         }
+      }
+   
    return 0;
 }
