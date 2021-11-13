1dc15d50f8c66322785f79109342910e15ff530f
manuelafedi <91663595+manuelafedi@users.noreply.github.com>
2021-11-13 20:46:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main() {
       }else{
          cout<<"1"<<endl;
          cout<<"1"<<endl;
-         int a=0, b=1, c=0;
-         for (int i=0; i<=(n=2); i++);
+         int a=1, b=1, c=0;
+         for (int i=2; i<=(n); i++);
          { c=a+b;
            a=b;
            b=c;
