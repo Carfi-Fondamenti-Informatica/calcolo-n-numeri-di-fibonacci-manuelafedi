f986c6e1200e45fd9babc8de9a476773e95fd7c7
manuelafedi <91663595+manuelafedi@users.noreply.github.com>
2021-11-13 20:56:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,11 +12,11 @@ int main() {
          cout<<"1"<<endl;
          cout<<"1"<<endl;
          int a=1, b=1, c=0;
-         for (int i=2; i<=(n); i++);
+         for [int i=2 i<=(n) i++);
          { c=a+b;
            a=b;
            b=c;
-          cout<<"c"<<endl;
+          cout<<"c"<<endl];
       }
    
    return 0;
