91ee8af42b27632345f5213bcf7937c826ece0bb
manuelafedi <91663595+manuelafedi@users.noreply.github.com>
2021-11-14 16:56:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 
-using namespace std;
+using namespace std; 
 
-int main() {
-
-int n=0, primo=1, secondo=1, fibonacci=0;
-
-cin>> n;
-
-        if(n<=2){
-            cout<<"errore"<<endl;
-        }else{
-            cout<< "1" << "\n" << "1" << endl;
-
-            for (int i = 2; i < n; ++i) {
-                fibonacci=primo+secondo;
-                primo=secondo;
-                secondo=fibonacci;
-                cout<< fibonacci <<endl;
-            }
-        }
-
-    return 0;
-}
+int main()
+{
+        int n,i=1,b=1,a=0,c;
+        cin>>n;
+        
+        
+        if(n>=2)
+        {
+                cout<<b<<endl;
+                for(i=2;i<=n;i++)
+                {
+                        
+                        c=a+b;
+                        cout<<c<<endl;
+                        a=b;
+                        b=c;
+                        
+                }
+                                
+                                
+                                
+                        }
+                        else
+                        {
+                                cout<<"errore"<<endl;
+                        }
+                        
+                        return 0;
+                }
